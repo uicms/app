2dbdf8d677847f4df068eefa814588d49f02899d
test <t.rolin@gmail.com>
2020-11-23 01:08:05 +0100

    Add blocks & medias to bootstrap db

--- install/db/bootstrap/mysql/bootstrap.sql
+++ install/db/bootstrap/mysql/bootstrap.sql
@@ -1,19 +1,84 @@
--- MySQL dump 10.16  Distrib 10.1.41-MariaDB, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.16  Distrib 10.1.47-MariaDB, for debian-linux-gnu (x86_64)
 --
--- Host: localhost    Database: symfony
+-- Host: localhost    Database: bootstrap
 -- ------------------------------------------------------
--- Server version	10.1.41-MariaDB-0+deb9u1
+-- Server version	10.1.47-MariaDB-0+deb9u1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES UTF8 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `block`
+--
+
+DROP TABLE IF EXISTS `block`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `published` datetime DEFAULT NULL,
+  `is_concealed` tinyint(1) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
+  `is_dir` tinyint(1) NOT NULL,
+  `position` int(11) NOT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_831B9722A76ED395` (`user_id`),
+  KEY `IDX_831B9722727ACA70` (`parent_id`),
+  CONSTRAINT `FK_831B9722727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `block` (`id`),
+  CONSTRAINT `FK_831B9722A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `block`
+--
+
+LOCK TABLES `block` WRITE;
+/*!40000 ALTER TABLE `block` DISABLE KEYS */;
+/*!40000 ALTER TABLE `block` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `block_translation`
+--
+
+DROP TABLE IF EXISTS `block_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `block_translation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `translatable_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `text` longtext COLLATE utf8mb4_unicode_ci,
+  `locale` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `block_translation_unique_translation` (`translatable_id`,`locale`),
+  KEY `IDX_6E6410B42C2AC5D3` (`translatable_id`),
+  CONSTRAINT `FK_6E6410B42C2AC5D3` FOREIGN KEY (`translatable_id`) REFERENCES `block` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `block_translation`
+--
+
+LOCK TABLES `block_translation` WRITE;
+/*!40000 ALTER TABLE `block_translation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `block_translation` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `doctrine_migration_versions`
@@ -30,6 +95,186 @@ CREATE TABLE `doctrine_migration_versions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+LOCK TABLES `doctrine_migration_versions` WRITE;
+/*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
+/*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `link_block_media`
+--
+
+DROP TABLE IF EXISTS `link_block_media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `link_block_media` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `block_id` int(11) DEFAULT NULL,
+  `media_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `published` datetime DEFAULT NULL,
+  `is_concealed` tinyint(1) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
+  `is_dir` tinyint(1) NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_60BDEBB9A76ED395` (`user_id`),
+  KEY `IDX_60BDEBB9727ACA70` (`parent_id`),
+  KEY `IDX_60BDEBB9E9ED820C` (`block_id`),
+  KEY `IDX_60BDEBB9EA9FDD75` (`media_id`),
+  CONSTRAINT `FK_60BDEBB9727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `link_block_media` (`id`),
+  CONSTRAINT `FK_60BDEBB9A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_60BDEBB9E9ED820C` FOREIGN KEY (`block_id`) REFERENCES `block` (`id`),
+  CONSTRAINT `FK_60BDEBB9EA9FDD75` FOREIGN KEY (`media_id`) REFERENCES `media` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `link_block_media`
+--
+
+LOCK TABLES `link_block_media` WRITE;
+/*!40000 ALTER TABLE `link_block_media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `link_block_media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `link_page_block`
+--
+
+DROP TABLE IF EXISTS `link_page_block`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `link_page_block` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `page_id` int(11) DEFAULT NULL,
+  `block_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `published` datetime DEFAULT NULL,
+  `is_concealed` tinyint(1) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
+  `is_dir` tinyint(1) NOT NULL,
+  `position` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_B210A801A76ED395` (`user_id`),
+  KEY `IDX_B210A801727ACA70` (`parent_id`),
+  KEY `IDX_B210A801C4663E4` (`page_id`),
+  KEY `IDX_B210A801E9ED820C` (`block_id`),
+  CONSTRAINT `FK_B210A801727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `link_page_block` (`id`),
+  CONSTRAINT `FK_B210A801A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_B210A801C4663E4` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`),
+  CONSTRAINT `FK_B210A801E9ED820C` FOREIGN KEY (`block_id`) REFERENCES `block` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `link_page_block`
+--
+
+LOCK TABLES `link_page_block` WRITE;
+/*!40000 ALTER TABLE `link_page_block` DISABLE KEYS */;
+/*!40000 ALTER TABLE `link_page_block` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media`
+--
+
+DROP TABLE IF EXISTS `media`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  `published` datetime DEFAULT NULL,
+  `is_concealed` tinyint(1) NOT NULL,
+  `is_locked` tinyint(1) NOT NULL,
+  `is_dir` tinyint(1) NOT NULL,
+  `position` int(11) NOT NULL,
+  `file` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_6A2CA10CA76ED395` (`user_id`),
+  KEY `IDX_6A2CA10C727ACA70` (`parent_id`),
+  CONSTRAINT `FK_6A2CA10C727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `media` (`id`),
+  CONSTRAINT `FK_6A2CA10CA76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media`
+--
+
+LOCK TABLES `media` WRITE;
+/*!40000 ALTER TABLE `media` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `media_translation`
+--
+
+DROP TABLE IF EXISTS `media_translation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `media_translation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `translatable_id` int(11) DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `locale` varchar(5) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `media_translation_unique_translation` (`translatable_id`,`locale`),
+  KEY `IDX_430137FC2C2AC5D3` (`translatable_id`),
+  CONSTRAINT `FK_430137FC2C2AC5D3` FOREIGN KEY (`translatable_id`) REFERENCES `media` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `media_translation`
+--
+
+LOCK TABLES `media_translation` WRITE;
+/*!40000 ALTER TABLE `media_translation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `media_translation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `migration_versions`
+--
+
+DROP TABLE IF EXISTS `migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `migration_versions`
+--
+
+LOCK TABLES `migration_versions` WRITE;
+/*!40000 ALTER TABLE `migration_versions` DISABLE KEYS */;
+INSERT INTO `migration_versions` VALUES ('DoctrineMigrations\\Version20201122235258','2020-11-23 00:52:59',3451),('DoctrineMigrations\\Version20201122235755','2020-11-23 00:57:56',3459),('DoctrineMigrations\\Version20201123000124','2020-11-23 01:01:25',5721),('DoctrineMigrations\\Version20201123000150','2020-11-23 01:01:51',5548);
+/*!40000 ALTER TABLE `migration_versions` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `page`
@@ -59,7 +304,7 @@ CREATE TABLE `page` (
   KEY `IDX_140AB620727ACA70` (`parent_id`),
   CONSTRAINT `FK_140AB620727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `page` (`id`),
   CONSTRAINT `FK_140AB620A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,6 +313,7 @@ CREATE TABLE `page` (
 
 LOCK TABLES `page` WRITE;
 /*!40000 ALTER TABLE `page` DISABLE KEYS */;
+INSERT INTO `page` VALUES (1,'html','index',NULL,'accueil',0,'menu','0',1,NULL,'2020-11-23 00:50:16','2020-11-23 00:59:53','2020-11-23 00:50:16',0,0);
 /*!40000 ALTER TABLE `page` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +334,7 @@ CREATE TABLE `page_translation` (
   UNIQUE KEY `page_translation_unique_translation` (`translatable_id`,`locale`),
   KEY `IDX_A3D51B1D2C2AC5D3` (`translatable_id`),
   CONSTRAINT `FK_A3D51B1D2C2AC5D3` FOREIGN KEY (`translatable_id`) REFERENCES `page` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,6 +343,7 @@ CREATE TABLE `page_translation` (
 
 LOCK TABLES `page_translation` WRITE;
 /*!40000 ALTER TABLE `page_translation` DISABLE KEYS */;
+INSERT INTO `page_translation` VALUES (1,1,'fr','Accueil',NULL);
 /*!40000 ALTER TABLE `page_translation` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,3 +397,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
+-- Dump completed on 2020-11-23  1:06:42
