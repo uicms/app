fa0d1659b50be39e435563a62cb0efb5dd324b6b
Timothée Rolin <t.rolin@gmail.com>
2022-05-16 15:22:49 +0200

    Make defaultController uses template specified in page

--- install/src/Controller/DefaultController.php
+++ install/src/Controller/DefaultController.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Uicms\App\Service\Model;
+
 class DefaultController extends AbstractController
 {
-    public function index($page)
+    public function index($page, Model $model)
     {
+        $template = $page->getTemplate() ? $page->getTemplate() : 'index';
+        
 		return $this->render(
-		            'app/tpl/default/index.html.twig',
-		            ['page'=>$page]
+		            'app/tpl/default/' . $template . '.html.twig',
+		            ['page'=>$page, 'medias'=>$model->get('Media')->getAll()]
 		        );
     }
 }
