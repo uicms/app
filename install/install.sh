b013437659cffdf55e77330c4eb8b7b141987f63
Timothée Rolin <t.rolin@gmail.com>
2023-09-26 16:58:20 +0200

    Fix install.sh

--- install/install.sh
+++ install/install.sh
@@ -11,12 +11,15 @@ fi
 if [ ! -d "./src/Services" ]; then
     mkdir ./src/Services
 fi
+
 cp ./vendor/uicms/app/install/bin/entity ./bin/
 cp ./vendor/uicms/app/install/bin/migrate ./bin/
+
 cp -r ./vendor/uicms/app/install/src/EventListener ./src/
 cp -r ./vendor/uicms/app/install/src/Command ./src/
+
 rsync -avz --delete ./vendor/uicms/app/install/config/ ./config/
 rsync -avz --delete ./vendor/uicms/app/install/src/Controller/ ./src/Controller/
 rsync -avz --delete ./vendor/uicms/app/install/src/Entity/ ./src/Entity/
 rsync -avz --delete ./vendor/uicms/app/install/src/Repository/ ./src/Repository/
-rsync -avz --delete ./vendor/uicms/app/install/src/Services/ ./src/Services/
+rsync -avz --delete ./vendor/uicms/app/install/src/Service/ ./src/Service/
